ac9fd7b06f893c3917120f8d9c76751f6dd969cf
huzhengen <huzhengen@163.com>
2018-01-11 17:00:51 +0800

    update

---
+++ react-music/src/components/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+// import './App.css';
+import './App.styl';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Welcome to React123</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
