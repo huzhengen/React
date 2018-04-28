840057f7dee55c63eaf92a82791a3cf1e2e4d6b1
huzhengen <huzhengen@163.com>
2018-04-28 10:24:20 +0800

    Comment.js

--- react-book/src/Comment.js
+++ react-book/src/Comment.js
@@ -1,13 +1,18 @@
 import React, {Component} from 'react'
+import PropTypes from 'prop-types'
 
 class Comment extends Component{
+	static propTypes = {
+		comment: PropTypes.object
+	}
 	render(){
+		const {comment} = this.props
 		return(
 			<div className="comment">
 				<div className="comment-user">
-					<span>用户名：{this.props.comment.username}</span>
+					<span>用户名：{comment.username}</span>
 				</div>
-				<p>评论：{this.props.comment.content}</p>
+				<p>评论：{comment.content}</p>
 			</div>
 		)
 	}
