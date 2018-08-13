a11f6ee75908e2fcf05bbd21e6b9c9cdb7b5dd2a
huzhengen <huzhengen@163.com>
2018-08-13 08:24:16 +0800

    App.js

--- react-movie/src/App/App.js
+++ react-movie/src/App/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-// import './App.css';
+import './App.css';
 import Index from '../component/index/index.js';
+import Search from '../component/search/search.js';
+import Detail from '../component/detail/detail.js';
 
 class App extends Component{
 
@@ -14,6 +16,9 @@ class App extends Component{
                             <p><Link to="/">首页</Link></p>
                         </div>
                         <Route exact path="/" component={Index} />
+                        <Route exact path="/search/" component={Search} />
+                        <Route exact path="/search/:movieName" component={Search} />
+                        <Route exact path="/detail/:movieId" component={Detail} />
                     </div>
                 </Router>
             </div>
